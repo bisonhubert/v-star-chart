618faa3a9d44b6bf55e8d3ba510289cbc1cecaa6
Bison Hubert <bisonhubert@gmail.com>
2022-03-13 15:52:41 -0700

    Add runner class

--- runner.py
+++ runner.py
@@ -1,17 +1,27 @@
 from page import Page
 from parser import PlanetParser
 
-page = Page(url='https://astrolibrary.org/free-birth-chart/').run()
-parser = PlanetParser(html=page.report_html).run()
-filepath = 'charts'
-extension = 'md'
 
-def make_files(planets):
-    for planet in planets:
-        filename = f"{filepath}/{planet.get('name')}.{extension}"
-        f = open(filename, "w")
-        f.write(planet.get('content'))
-        f.close()
+class Runner:
+    FILEPATH = 'charts'
+    EXTENSION = 'md'
 
-make_files(parser.planets)
-import pdb;pdb.set_trace()
+    def __init__(self, url=None):
+        self.page = Page(url=url).run()
+
+    def _make_files(self, planets=None):
+        if planets is None:
+            return
+        for planet in planets:
+            filename = f"{self.FILEPATH}/{planet.get('name')}.{self.EXTENSION}"
+            f = open(filename, "w")
+            f.write(planet.get('content'))
+            f.close()
+
+    def run(self):
+        parser = PlanetParser(html=self.page.report_html).run()
+        self._make_files(planets=parser.planets)
+
+
+runner = Runner(url='https://astrolibrary.org/free-birth-chart/')
+runner.run()
