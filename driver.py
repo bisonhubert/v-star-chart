a0d8cde7b93ed3c0a79534714554b1c9b0fc1291
Bison Hubert <bisonhubert@gmail.com>
2022-03-13 15:27:25 -0700

    Add report

---
+++ driver.py
@@ -0,0 +1,72 @@
+import time
+
+from selenium.webdriver import Firefox
+from selenium.webdriver.support.select import Select
+
+class PageObj:
+    def __init__(self, url=None):
+        self.url = url
+        self.browser = None
+        self.report = None # selenium WebElement obj
+        self.report_html = None # selenium WebElement innerHTML
+
+    def _set_name(self):
+        name = self.browser.find_element_by_id('name')
+        name.send_keys('Vanessa')
+
+    def _set_month(self, month=3):
+        month_selector = Select(self.browser.find_element_by_id('month'))
+        month_selector.select_by_index(month)
+
+    def _set_day(self, day=23):
+        day_selector = Select(self.browser.find_element_by_id('day'))
+        day_selector.select_by_index(day)
+
+    def _set_year(self):
+        year = self.browser.find_element_by_id('year')
+        year.send_keys('1989')
+
+    def _set_hour(self, hour=None):
+        if hour is None:
+            hour = 1
+        hour_selector = Select(self.browser.find_element_by_id('hour'))
+        hour_selector.select_by_index(hour)
+
+    def _set_minute(self, minute=None):
+        if minute is None:
+            minute = 1
+        minute_selector = Select(self.browser.find_element_by_id('minute'))
+        minute_selector.select_by_index(minute)
+
+    def _set_location(self):
+        location = self.browser.find_element_by_id('placein')
+        location.send_keys("Galveston")
+        time.sleep(3)
+        location_results = self.browser.find_element_by_id('autoComplete_results_list')
+        location_results.find_elements_by_tag_name('li')[0].click()
+
+    def _submit(self):
+        submit = self.browser.find_element_by_id('z-fetch-report')
+        submit.click()
+
+    def _get_page(self):
+        self.browser = Firefox()
+        self.browser.get(self.url)
+
+    def _set_report(self):
+        report = self.browser.find_element('id', 'zp-report-content')
+        self.report = report
+        self.report_html = report.get_attribute('innerHTML')
+
+    def run(self, hour=None, minute=None):
+        self._get_page()
+        self._set_name()
+        self._set_month()
+        self._set_day()
+        self._set_year()
+        self._set_hour(hour=hour)
+        self._set_minute(minute=minute)
+        self._set_location()
+        self._submit()
+        self._set_report()
+        return self
--- runner.py
+++ runner.py
@@ -1,57 +1,7 @@
-import time
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
 
-from selenium.webdriver import Firefox
-from selenium.webdriver.support.select import Select
+from driver import PageObj
 
-url = 'https://astrolibrary.org/free-birth-chart/'
-
-
-browser = Firefox()
-browser.get(url)
-
-class PageObj:
-    name = browser.find_element_by_id('name')
-    month = Select(browser.find_element_by_id('month'))
-    day = Select(browser.find_element_by_id('day'))
-    year = browser.find_element_by_id('year')
-    hour = Select(browser.find_element_by_id('hour'))
-    minute = Select(browser.find_element_by_id('minute'))
-    location = browser.find_element_by_id('placein')
-    location_results = browser.find_element_by_id('autoComplete_results_list')
-    submit = browser.find_element_by_id('z-fetch-report')
-
-    def _set_name(self):
-        self.name.send_keys('Vanessa')
-
-    def _set_month(self):
-        self.month.select_by_index(3)
-
-    def _set_day(self):
-        self.day.select_by_index(23)
-
-    def _set_year(self):
-        self.year.send_keys('1989')
-
-    def _set_hour(self, hour=1):
-        self.hour.select_by_index(hour)
-
-    def _set_minute(self, minute=1):
-        self.minute.select_by_index(minute)
-
-    def _set_location(self):
-        self.location.send_keys("Galveston")
-        time.sleep(3)
-        self.location_results.find_elements_by_tag_name('li')[0].click()
-
-    def run(self, hours=None, minutes=None):
-        self._set_name()
-        self._set_month()
-        self._set_day()
-        self._set_year()
-        self._set_hour()
-        self._set_minute()
-        self._set_location()
-        self.submit.click()
-
-PageObj().run()
+page_obj = PageObj(url='https://astrolibrary.org/free-birth-chart/').run()
 import pdb;pdb.set_trace()
