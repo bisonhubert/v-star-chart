b6919bbad958d056ba99051c2bf53a1d4e83e09c
Bison Hubert <bisonhubert@gmail.com>
2022-03-13 16:22:09 -0700

    Run for ranges

--- runner.py
+++ runner.py
@@ -1,27 +1,42 @@
 from page import Page
 from parser import PlanetParser
 
+# HOUR_RANGE = list(range(12, 25))
+# MINUTES = list(range(1, 61))
+URL = 'https://astrolibrary.org/free-birth-chart/'
+HOURS = list(range(12, 14))
+MINUTES = list(range(1, 3))
+
 
 class Runner:
     FILEPATH = 'charts'
     EXTENSION = 'md'
 
-    def __init__(self, url=None):
-        self.page = Page(url=url).run()
+    def __init__(self, hours=None, minutes=None):
+        self.hours = None if hours is None else hours
+        self.minutes = None if minutes is None else minutes
 
-    def _make_files(self, planets=None):
+    def _make_files(self, hour=None, minute=None, planets=None):
         if planets is None:
             return
+        timestamp = ''
+        if hour is not None and minute is not None:
+            timestamp = f'{hour:02}{minute:02}'
         for planet in planets:
-            filename = f"{self.FILEPATH}/{planet.get('name')}.{self.EXTENSION}"
-            f = open(filename, "w")
+            filename = f"{timestamp}_{planet.get('name')}.{self.EXTENSION}"
+            f = open(f"{self.FILEPATH}/{filename}", "w")
             f.write(planet.get('content'))
             f.close()
 
-    def run(self):
-        parser = PlanetParser(html=self.page.report_html).run()
-        self._make_files(planets=parser.planets)
+    def run(self, url=None):
+        if url is None:
+            return self
+        for hour in self.hours:
+            for minute in self.minutes:
+                page = Page(url=url).run(hour=hour, minute=minute)
+                parser = PlanetParser(html=page.report_html).run()
+                self._make_files(hour=hour, minute=minute, planets=parser.planets)
+        return self
 
 
-runner = Runner(url='https://astrolibrary.org/free-birth-chart/')
-runner.run()
+runner = Runner(hours=HOURS, minutes=MINUTES).run(url=URL)
