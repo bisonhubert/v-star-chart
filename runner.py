4d2dae0bdc4761b9df5e0aaf5a38870a15ca0d63
Bison Hubert <bisonhubert@gmail.com>
2022-03-13 17:51:15 -0700

    Update filename

--- runner.py
+++ runner.py
@@ -3,11 +3,12 @@ import time
 from page import Page
 from parser import PlanetParser
 
-# HOUR_RANGE = list(range(12, 25))
-# MINUTES = list(range(1, 61))
+HOURS = list(range(12, 16))
+MINUTES = list(range(1, 61))
 URL = "https://astrolibrary.org/free-birth-chart/"
-HOURS = list(range(12, 14))
-MINUTES = list(range(1, 3))
+# test with these settings
+# HOURS = list(range(12, 14))
+# MINUTES = list(range(1, 3))
 
 
 class Runner:
@@ -23,7 +24,10 @@ class Runner:
             return
         timestamp = ""
         if hour is not None and minute is not None:
-            timestamp = f"{hour:02}{minute:02}"
+            # hour and minute are indexed for page selector
+            # formatting for actual hour/minute
+            # ie: if hour = 12, hour = 11AM
+            timestamp = f"{hour-1:02}{minute-1:02}"
         for planet in planets:
             filename = f"{timestamp}_{planet.get('name')}.{self.EXTENSION}"
             f = open(f"{self.FILEPATH}/{filename}", "w")
